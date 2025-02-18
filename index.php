49cc14424c143bdccfe90255c5faa93242db5083
Marvin3312 <marvinhernandez2000@gmail.com>
2025-02-17 23:57:48 -0600

    Update: for estructuration the proyect

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./webs/views/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="webs/views/assets/css/index.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+include 'webs/views/pages/main/features.php';
+include 'webs/views/pages/main/pricing.php';
+include 'webs/views/pages/main/products.php';
+
+
+?>
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
